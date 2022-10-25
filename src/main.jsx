12f7ff0fd46ccde9d82e73fc3585e4b0d2ffa4a2
Raihan Adam <hallwack.id@gmail.com>
2022-10-25 15:12:17 +0700

    feat: add react-router-dom configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import CharacterDetail from "./pages/CharacterDetail";
+import CharacterList from "./pages/CharacterList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/characters",
+    element: <CharacterList />,
+  },
+  {
+    path: "/characters/:id",
+    element: <CharacterDetail />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
