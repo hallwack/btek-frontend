a9e86e0746f05d514630ff91cd9a36a53de40297
Raihan Adam <hallwack.id@gmail.com>
2022-10-26 17:22:09 +0700

    feat: add login route and require auth component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider, useLocation } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import CharacterDetail from "./pages/CharacterDetail";
 import CharacterList from "./pages/CharacterList";
+import Login from "./pages/Login";
+
+const RequireAuth = ({ children }) => {
+  const location = useLocation()
+  const a = true
+
+  if (a) {
+    return <Navigate to="/login" state={{ from: location }} />
+  }
+
+  return children
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <RequireAuth><App /></RequireAuth>
   },
   {
     path: "/characters",
@@ -18,6 +30,10 @@ const router = createBrowserRouter([
   {
     path: "/characters/:id",
     element: <CharacterDetail />
+  },
+  {
+    path: "/login",
+    element: <Login />
   }
 ])
 
