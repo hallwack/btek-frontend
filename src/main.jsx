8af93d4d12e54c13be0397e5018290e1d3eb9438
Raihan Adam <hallwack.id@gmail.com>
2022-10-27 18:12:12 +0700

    fix: fixing RequireAuth component

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Navigate, RouterProvider, useLocation } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import CharacterDetail from "./pages/CharacterDetail";
 import CharacterList from "./pages/CharacterList";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const RequireAuth = ({ children }) => {
-  const location = useLocation()
-  const a = true
+  const location = useLocation();
+  const token = window.localStorage.getItem("token");
 
-  if (a) {
-    return <Navigate to="/login" state={{ from: location }} />
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} />;
   }
 
-  return children
-}
+  return children;
+};
 
-const router = createBrowserRouter([
+const routerMain = createBrowserRouter([
   {
     path: "/",
-    element: <RequireAuth><App /></RequireAuth>
+    element: (
+      <RequireAuth>
+        <App />
+      </RequireAuth>
+    ),
   },
   {
     path: "/characters",
@@ -29,16 +39,20 @@ const router = createBrowserRouter([
   },
   {
     path: "/characters/:id",
-    element: <CharacterDetail />
+    element: <CharacterDetail />,
   },
   {
     path: "/login",
-    element: <Login />
-  }
-])
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={routerMain} />
   </React.StrictMode>
 );
