d8a9e727da9594725aee321556b33aa5d35945da
Raihan Adam <hallwack.id@gmail.com>
2022-10-27 18:12:26 +0700

    feat: add logout button with logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./styles/App.css";
 
 const App = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    window.localStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
     <div>
       <h1>Home</h1>
       <Link to="/characters">Characters</Link>
+      <button
+        type="submit"
+        onClick={handleLogout}
+        className="px-3 py-2 bg-sky-300 rounded-md"
+      >
+        Logout
+      </button>
     </div>
   );
 };
