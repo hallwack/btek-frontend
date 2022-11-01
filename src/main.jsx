222857a2b462d095c5f6c0a239134d04ce369312
Raihan Adam <hallwack.id@gmail.com>
2022-11-01 20:06:32 +0700

    fix: fixing routing

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const RequireAuth = ({ children }) => {
   const token = window.localStorage.getItem("token");
 
   if (!token) {
-    return <Navigate to="/login" state={{ from: location }} />;
+    return <Navigate to="/auth/login" state={{ from: location }} />;
   }
 
   return children;
