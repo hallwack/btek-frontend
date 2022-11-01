e644eb45c715bdaf03a86eb88306ec76c9b48542
Raihan Adam <hallwack.id@gmail.com>
2022-11-01 20:06:17 +0700

    feat: add daisyui config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#9eb6ed",
+          secondary: "#66f480",
+          accent: "#5e6cc9",
+          neutral: "#2E2833",
+          "base-100": "#FAFAFA",
+          info: "#469BC8",
+          success: "#71EAA3",
+          warning: "#F5BD2E",
+          error: "#F7472B",
+        },
+      },
+    ],
+  },
 };
