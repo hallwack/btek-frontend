d6cca00bdfa833d72b9e469a3070321d9a564408
Raihan Adam <hallwack.id@gmail.com>
2022-11-04 16:05:48 +0700

    feat: add pending action

--- src/redux/reducers/profile.js
+++ src/redux/reducers/profile.js
@@ -5,6 +5,7 @@ import * as authAction from "../asyncActions/auth";
 const initialState = {
   user: {},
   token: "",
+  isLoading: false,
 };
 
 const profile = createSlice({
@@ -12,19 +13,31 @@ const profile = createSlice({
   initialState,
   reducers: {
     handleReset: (state) => {
-      state.user = initialState;
+      state.user = initialState.user;
       state.token = initialState.token;
     },
   },
   extraReducers: (builder) => {
+    builder.addCase(profileAction.getDataUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
     builder.addCase(profileAction.getDataUser.fulfilled, (state, action) => {
       state.user = action.payload.results;
+      state.isLoading = false;
+    });
+    builder.addCase(profileAction.editData.pending, (state, action) => {
+      state.isLoading = true;
     });
     builder.addCase(profileAction.editData.fulfilled, (state, action) => {
       state.user = action.payload.results;
+      state.isLoading = false;
+    });
+    builder.addCase(authAction.getToken.pending, (state, action) => {
+      state.isLoading = true;
     });
     builder.addCase(authAction.getToken.fulfilled, (state, action) => {
       state.token = action.payload.results.token;
+      state.isLoading = false;
     });
   },
 });
