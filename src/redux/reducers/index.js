8da79b6948783fec094d9f3854d6c06d56765ee7
Raihan Adam <hallwack.id@gmail.com>
2022-11-04 15:20:25 +0700

    feat: add auth reducer to combineReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import profile from "./profile";
+import auth from "./auth";
 
 const reducer = combineReducers({
   profile,
+  auth,
 });
 
 export default reducer;
